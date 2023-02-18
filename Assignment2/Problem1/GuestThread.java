b607730b0b16b0cbe3eb239efd3129f2aaa7fd01
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2023-02-18 11:35:21 -0500

    Simulating Problem1 winning strategy with prints

--- Assignment2/Problem1/GuestThread.java
+++ Assignment2/Problem1/GuestThread.java
@@ -13,14 +13,58 @@ public class GuestThread implements Runnable {
     // ====================================== Class Variables ======================================
     private int threadNumber;
 
+    // Boolean is set to true if a guest has previously eaten cake
+    private boolean ateCake;
+
     // ========================================= Functions =========================================
-    public GuestThread(int threadNumber) {
+
+    public GuestThread(int threadNumber, boolean ateCake) {
         this.threadNumber = threadNumber;
-        }
+        this.ateCake = ateCake;
+    }
 
     @Override
     public synchronized void run() {
         System.out.println("Thread (" + threadNumber + ") run.");
+
+        if (threadNumber == MinotaursBirthday.firstGuest) {
+            System.out.println("======================  FIRST GUEST (" + threadNumber + ") ====================== ");
+            
+            if (MinotaursBirthday.cakeOnTable == false) {
+                System.out.println("Placing new cake on table.");
+                MinotaursBirthday.cakeOnTable = true;
+                MinotaursBirthday.firstGuestCakeReset++;
+
+                // Checks if all other guests (excluding the first guest) have entered the maze
+                if(MinotaursBirthday.firstGuestCakeReset == (MinotaursBirthday.numOfGuests - 1)) {
+                    
+                    // All other guests have entered maze, first guest can now eat cake and end party
+                    System.out.println("Can now eat cake.");
+                    MinotaursBirthday.guestAteCake[threadNumber] = true;
+
+                    // Increasing firstGuestCakeReset once more to end party
+                    MinotaursBirthday.firstGuestCakeReset++;
+                }
+            }
+            System.out.println("firstGuestCakeReset: " + MinotaursBirthday.firstGuestCakeReset);
+            System.out.println("========================================================== ");
+        }
+        else {
+            if (MinotaursBirthday.cakeOnTable == true) {
+                if (ateCake == false) {
+                    this.ateCake = true;
+                    MinotaursBirthday.guestAteCake[threadNumber] = ateCake;
+                    MinotaursBirthday.cakeOnTable = false;
+                    System.out.println("Guest (" + threadNumber + ") ate cake set to " + ateCake + ".");
+                }
+                else {
+                    System.out.println("Guest (" + threadNumber + ") previously ate cake.");
+                }
+            }
+            else {
+                System.out.println("Guest (" + threadNumber + ") leaving table as is. No cake on table.");
+            }
+        }
     }
 
 }
--- Assignment2/Problem1/MinotaursBirthday.java
+++ Assignment2/Problem1/MinotaursBirthday.java
@@ -9,20 +9,44 @@
 //       Party.
 
 import java.util.*;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
 
 public class MinotaursBirthday {
 
+    // ====================================== Class Variables ======================================
+    public static int firstGuest = 0;
+    public static int numOfGuests =  100;
+    private static int guestInMaze = 0;
+
+    // Boolean array where guestAteCake[i] == false when guest i has eaten cake
+    public static boolean[] guestAteCake = new boolean[numOfGuests]; 
+
+    // Variable keeps track of how many times the first guest has reset the cake
+    public static int firstGuestCakeReset = 0;
+
+    // Boolean determines if cupcake is on the table at end of maze
+    public static boolean cakeOnTable = true;
+
+    // ========================================= Functions =========================================
     public static void main(String[] args) {
-        
-        // Takes number of guests (integer) as input
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How many guests are at the Minotaur's Party: ");
-        int numOfGuests =  sc.nextInt();
+
+        // Starts timer
+        long start = System.nanoTime();
+
+        // Creates randomizer from 0 to numOfGuests-1
+        Random rand = new Random(); 
+        guestInMaze = rand.nextInt(numOfGuests);
+
+        // Sets firstGuest in maze
+        firstGuest = guestInMaze;
+        System.out.println("First guest is: " + firstGuest);
 
         // Creating a new thread for each guest at the Minotaur's Party
-        for (int i = 0; i < numOfGuests; i++)
+        while (firstGuestCakeReset != numOfGuests)
         {
-            GuestThread guestThread = new GuestThread(i);
+            // Creates and joins new guest thread
+            GuestThread guestThread = new GuestThread(guestInMaze, guestAteCake[guestInMaze]);
             Thread newThread = new Thread(guestThread);
             newThread.start();
 
@@ -32,7 +56,20 @@ public class MinotaursBirthday {
                 System.out.println("\nAn error occured joning a thread.");
             }
 
+            // Gets next guest to enter maze
+            guestInMaze = rand.nextInt(numOfGuests);
         }
+
+        // Ends timer
+        long end = System.nanoTime();
+
+        // Prints execution time
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.println("Execution time: " + formatter.format((end - start) / 1000000000d) + "s");
+
+        // Prints guestsAteCake to assure all guests ate
+        System.out.println("\nguestsAteCake[" + numOfGuests + "]: ");
+        System.out.println(Arrays.toString(guestAteCake) + "\n");
 
     }
 }
