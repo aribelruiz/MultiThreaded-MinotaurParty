c596a0566de0ad68442517b994c3f8468aeeeaf8
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2023-02-21 15:06:07 -0500

    Simulating strategy 2 in Problem 2

--- Assignment2/Problem2/GuestThread.java
+++ Assignment2/Problem2/GuestThread.java
@@ -7,11 +7,16 @@
 //      This program represents each guest checking the showroom to see a crystal vase.
 
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.TimeUnit;
 
 public class GuestThread implements Runnable {
 
     // ====================================== Class Variables ======================================
     private int threadNumber;
+    private boolean lockAcquired;
+
+    private boolean firstTimeGuest = false;
 
     // ========================================= Functions =========================================
 
@@ -21,6 +26,38 @@ public class GuestThread implements Runnable {
 
     @Override
     public synchronized void run() {
-        System.out.println("Thread (" + threadNumber + ") run.");
+        try {
+            // System.out.println("Thread (" + threadNumber + ") run.");
+
+            // If showroom is available, let guest enter showroom
+            if (MinotaursVase.showroomSign == MinotaursVase.AVAILABLE) {
+                lockAcquired = MinotaursVase.lock.tryLock(1, TimeUnit.SECONDS);
+
+                if (lockAcquired) {
+                    try {
+                        // If guest acquires lock, set showroom to BUSY
+                        MinotaursVase.showroomSign = MinotaursVase.BUSY;
+    
+                        // Update guest number to have seen vase
+                        if (MinotaursVase.guestsSeenVaseArr[threadNumber] == false) {
+                            MinotaursVase.guestsSeenVaseArr[threadNumber] = true;
+                            firstTimeGuest = true;
+                        }
+                    } finally {
+                        // After seeing vase, set showroom to available and unlock lock
+                        MinotaursVase.showroomSign = MinotaursVase.AVAILABLE;
+                        MinotaursVase.lock.unlock();
+                        
+                        // Keeps track of how many guests have seen the vase
+                        if (firstTimeGuest)
+                            MinotaursVase.guestsSeenVase++;                        
+                    }
+                }
+            }
+            // Otherwise, showroom is busy and guest does not attempt to enter
+
+        } catch(InterruptedException e) {
+            System.out.println("An error occured");
+        }
     }
 }
--- Assignment2/Problem2/MinotaursVase.java
+++ Assignment2/Problem2/MinotaursVase.java
@@ -8,26 +8,99 @@
 //      This program simulates strategy 2 for Problem 2 of Assignment 2, Minotaur's Crystal Vase.
 
 import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MinotaursVase {
 
     // ====================================== Class Variables ======================================
 
-    const private int numOfGuests = 10;
+    private static int nextGuestInRoom = 0;
+    private static int numOfGuests = 10;
 
-    const private boolean AVAILABLE = true;
-    const private boolean BUSY = false;
+    public static int guestViewingVase;
+    public static int guestsSeenVase = 0;
+    public static boolean[] guestsSeenVaseArr = new boolean[numOfGuests];
 
-    public static boolean showroomSign = AVAILABLE; // possibly use lock
+    public final static boolean AVAILABLE = true;
+    public final static boolean BUSY = false;
+    public static boolean showroomSign = AVAILABLE; 
+
+
+    public static Lock lock = new ReentrantLock();
 
     // ========================================= Functions =========================================
     public static void main(String args[]) {
+        
+        // Starts timer
+        long start = System.nanoTime();
 
-        for (int i = 0; i < 10; i++) {
-            // Creates and joins new guest thread
-            GuestThread guestThread = new GuestThread(i);
-            Thread newThread = new Thread(guestThread);
-            newThread.start();
+        // Creates randomizer from 0 to numOfGuests-1
+        Random rand = new Random(); 
+        nextGuestInRoom = rand.nextInt(numOfGuests);
+
+        // Continues selecting guests who want to see the vase
+        while(guestsSeenVase != numOfGuests) {
+
+            // Creates new guest thread to enter showroom if showroom is available
+            if ((showroomSign == AVAILABLE) && (nextGuestInRoom != guestViewingVase)) {
+    
+                // Update guestViewing vase
+                guestViewingVase = nextGuestInRoom;
+                
+                // Allows guest to enter show room
+                GuestThread guestThread = new GuestThread(guestViewingVase);
+                Thread newThread = new Thread(guestThread);
+                newThread.start();
+            } 
+            else {
+                // Gets next guest who wants to check to see vase
+                nextGuestInRoom = rand.nextInt(numOfGuests);
+            }
+        }
+
+        // Ends timer
+        long end = System.nanoTime();
+
+        // Prints execution time
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.println("Execution time: " + formatter.format((end - start) / 1000000000d) + "s");
+
+        // Creates MinotaursVase.txt file to print output
+        try {
+            File outputFile = new File("MinotaursVase.txt");
+    
+            if (outputFile.createNewFile()) {
+                System.out.println("\nFile created: " + outputFile.getName()); 
+        }
+        else {
+            System.out.println("\nFile '" + outputFile.getName() + "' already exists.");
+        }
+
+        } catch (IOException e) {
+            System.out.println("\nAn error occured.");
+            e.printStackTrace();
+        }
+
+        // Writes output to MinotaursVase.txt file
+        try {
+            FileWriter myWriter = new FileWriter("MinotaursVase.txt");
+            
+            myWriter.write("\nguestsSeenVase[" + numOfGuests + "]: ");
+            myWriter.write(Arrays.toString(guestsSeenVaseArr) + "\n");
+           
+            // Closing file writer
+            myWriter.close();
+            System.out.println("Successfully wrote output to 'MinotaursVase.txt'.\n");
+                
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 }
